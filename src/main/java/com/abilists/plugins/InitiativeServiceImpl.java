90be49d403c13322624bd02512e33fd69683331c
kim joon <minziappa@gmail.com>
2020-03-12 03:49:58 +0900

    Update the configuration to initiative

--- src/main/java/com/abilists/plugins/InitiativeServiceImpl.java
+++ src/main/java/com/abilists/plugins/InitiativeServiceImpl.java
@@ -7,7 +7,6 @@ import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,7 +26,6 @@ public class InitiativeServiceImpl extends AbilistsAbstractService implements Pl
 	final Logger logger = LoggerFactory.getLogger(InitiativeServiceImpl.class);
 	final String INITIATIVE_SQL = "/sqlMap/plugins/initiativeSql.txt";
 	final String MY_DRIVER = "org.h2.Driver";
-//	final String MY_URL = "jdbc:h2:~/.abilists/h2db/v1.3/abilists;IGNORECASE=TRUE;DB_CLOSE_DELAY=10;MODE=MYSQL;AUTO_RECONNECT=TRUE;INIT=create schema if not exists abilists\\;SET SCHEMA abilists;AUTO_SERVER=TRUE";
 
 	@Override
 	public String createTables(HashMap<String, String> mapHash) throws Exception {
